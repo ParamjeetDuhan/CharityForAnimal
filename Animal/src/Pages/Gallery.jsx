2fb352134980021cc76f7d961452884c839a48a0
Paramjeet singh <paramjeetsingh16346@gmail.com>
2024-12-01 11:54:36 +0530

    push 10

--- Animal/src/Pages/Gallery.jsx
+++ Animal/src/Pages/Gallery.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import img1 from '../assets/dog1.jpg'
+import img2 from '../assets/dog2.webp'
+import img3 from '../assets/dog3.jpg'
+import img4 from '../assets/dog4.jpg'
+import img5 from '../assets/dog5.webp'
+import img6 from '../assets/cow1.jpg'
+import img7 from '../assets/cow2.jpg'
+import img8 from '../assets/cow3.jpg'
+import img9 from '../assets/cow4.jpg'
 
 function Gallery() {
   return (
-    <div>
-     <div className='container mx-auto p-4'>
-            <div className='grid grid-cols-1 gap-3 md:grid-cols-3 py-8'>
-                
+    <div className='bg-gradient-to-t from-black via-primary to-black mt-20'>
+     <div className='container mx-auto p-4 '>
+            <div className='grid grid-cols-1 gap-1 md:grid-cols-3 py-8 '>
+                <img src={img1} alt="" className='w-80 h-80 py-3'/>
+                <img src={img2} alt="" className='w-80 h-80 py-3'/>
+                <img src={img3} alt="" className='w-80 h-80 py-3'/>
+                <img src={img4} alt="" className='w-80 h-80 py-3'/>
+                <img src={img5} alt="" className='w-80 h-80 py-3'/>
+                <img src={img6} alt="" className='w-80 h-80 py-3'/>
+                <img src={img7} alt="" className='w-80 h-80 py-3'/>
+                <img src={img8} alt="" className='w-80 h-80 py-3'/>
+                <img src={img9} alt="" className='w-80 h-80 py-3'/>
             </div>
      </div>
     </div>
--- Animal/src/Pages/Services.jsx
+++ Animal/src/Pages/Services.jsx
@@ -2,8 +2,47 @@ import React from 'react'
 
 function Services() {
   return (
-    <div className='bg-yellow-500 w-screen h-screen'>
-      <h1>hello</h1>
+    <div className='bg-gradient-to-r from-black via-primary to-black mt-20 text-white'>
+      <div className='container mx-auto p-4'>
+      <div className='grid grid-cols-1 gap-3 md:grid-cols-3 py-8 '>
+          <div className='bg-yellow-600 w-fit h-fit rounded-md ml-5 my-4 '>
+            <h1 className='text-center text-black md:text-2xl sm:text-xl font-bold'>Basic Necessities</h1>
+            <p>Feeding Programs: Regular provision of nutritious food and clean water.</p>
+            <p>Shelter: Safe spaces for protection from harsh weather and predators.</p>
+            <p>Clothing/Protection: Warm coats or protective gear for cold or rainy weather.</p>
+          </div>
+          <div className='bg-yellow-600 w-fit h-fit rounded-md ml-5 my-4'>
+          <h1 className='text-center text-black md:text-2xl sm:text-xl font-bold'>Medical Care</h1>
+            <p>Vaccination: Regular immunizations to prevent diseases like rabies, distemper, and parvovirus.</p>
+            <p>De-worming: Treatments to eliminate internal parasites.</p>
+            <p>First Aid: Immediate treatment for injuries or wounds.</p>
+          </div>
+          <div className='bg-yellow-600 w-fit h-fit rounded-md ml-5 my-4'>
+          <h1 className='text-center text-black md:text-2xl sm:text-xl font-bold'>Rehabilitation and Behavioral Care</h1>
+            <p>Feeding Programs: Regular provision of nutritious food and clean water.</p>
+            <p>Shelter: Safe spaces for protection from harsh weather and predators.</p>
+            <p>Clothing/Protection: Warm coats or protective gear for cold or rainy weather.</p>
+          </div>
+          <div className='bg-yellow-600 w-fit h-fit rounded-md ml-5 my-4'>
+          <h1 className='text-center text-black md:text-2xl sm:text-xl font-bold'>Basic Necessities</h1>
+            <p>Feeding Programs: Regular provision of nutritious food and clean water.</p>
+            <p>Shelter: Safe spaces for protection from harsh weather and predators.</p>
+            <p>Clothing/Protection: Warm coats or protective gear for cold or rainy weather.</p>
+          </div>
+          <div className='bg-yellow-600 w-fit h-fit rounded-md ml-5 my-4'>
+          <h1 className='text-center text-black md:text-2xl sm:text-xl font-bold'>Basic Necessities</h1>
+            <p>Feeding Programs: Regular provision of nutritious food and clean water.</p>
+            <p>Shelter: Safe spaces for protection from harsh weather and predators.</p>
+            <p>Clothing/Protection: Warm coats or protective gear for cold or rainy weather.</p>
+          </div>
+          <div className='bg-yellow-600 w-fit h-fit rounded-md ml-5 my-4'>
+          <h1 className='text-center text-black md:text-2xl sm:text-xl font-bold'>Basic Necessities</h1>
+            <p>Feeding Programs: Regular provision of nutritious food and clean water.</p>
+            <p>Shelter: Safe spaces for protection from harsh weather and predators.</p>
+            <p>Clothing/Protection: Warm coats or protective gear for cold or rainy weather.</p>
+          </div>
+      </div>
+      </div>
     </div>
   )
 }
